0c3e66d5fa638f4e084e1b676739f9d15313443d
TanimShahriar <ssmtsshr@gmail.com>
2023-11-24 20:06:17 +0600

    server site iniatiazing with exress js, mongoDB

---
+++ index.js
@@ -0,0 +1,118 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const jwt = require("jsonwebtoken")
+require('dotenv').config()
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6mxxl.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+
+    const apartmentCollection = client.db('hillApartment').collection('apartments')
+
+
+
+    //jwt related api
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h"
+      });
+      res.send({ token });
+    })
+
+    //middlewares
+    const verifyToken = (req, res, next) => {
+      console.log("inside verify token", req.headers.authorization);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: "unauthorized access" })
+      }
+      // next();
+      const token = req.headers.authorization.split(" ")[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+        if (error) {
+          return res.status(401).send({ message: "unauthorized access" })
+        }
+        req.decoded = decoded;
+        next();
+      })
+      // if (!token) {
+      //   return res.status(401).send({ message: "forbidden access" })
+      // }
+    }
+    //use verify admin after verify token
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      const isAdmin = user?.role === "admin";
+      if (!isAdmin) {
+        return res.status(403).send({ message: "Forbidden access" })
+      }
+      next();
+    }
+
+
+    //user related api
+
+
+
+    //menu related api
+    app.get("/apartments", async (req, res) => {
+      const cursor = apartmentCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    // app.get("/review", async (req, res) => {
+    //   const cursor = reviewCollection.find();
+    //   const result = await cursor.toArray();
+    //   res.send(result);
+    // })
+
+    //cart related api
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+  res.send('Apartment boss is sitting')
+})
+
+app.listen(port, () => {
+  console.log(`Apartment is sitting on port: ${port}`)
+})
