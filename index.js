9c6d5bb718d62ef721f7804bf113446e93a54703
TanimShahriar <ssmtsshr@gmail.com>
2023-11-30 08:36:22 +0600

    coupons added and payment db upated

--- index.js
+++ index.js
@@ -378,6 +378,14 @@ async function run() {
       res.send(result);
     })
 
+    app.delete("/coupons/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await couponCollection.deleteOne(query);
+      res.send(result);
+    })
+
+
 
 
 
